472de9397e4dd81d288a8f37980730e4e3a95eb9
JadenFicklin <jadendficklin@gmail.com>
2021-12-17 11:23:28 -0700

    added more conditional statements to the password

--- password.js
+++ password.js
@@ -9,9 +9,19 @@ reader.question(
   "Welcome to the password validator tool, please enter a password to validate: ",
   function (input) {
     let passwordLength = input.length;
+    let includesSpace = input.includes(" ");
+    let includesPlus = input.includes("+");
 
     if (passwordLength < 10) {
       console.log("Password must be atleast 10 characters long");
+    } else if (passwordLength > 30) {
+      console.log("Password is too long");
+    } else if ((input = /\d/.test(input))) {
+      console.log("no numbers allowed in password");
+    } else if (includesSpace) {
+      console.log("no spaces allowed in password");
+    } else if (includesPlus) {
+      console.log("no plus sign allowed");
     } else {
       console.log("Success, password created");
     }
